fd4b9424445e8fe879f72046990314eec13e1370
Raven <ravend2013@gmail.com>
2025-06-24 11:13:52 +0100

    feat: removed autoclosing html tags

--- nvim/lua/plugins/autotag.lua
+++ nvim/lua/plugins/autotag.lua
@@ -1,4 +1,10 @@
 return {
   "windwp/nvim-ts-autotag",
-  opts = {},
+  opts = function()
+    require('nvim-ts-autotag').setup({
+      opts = {
+        enable_close = false,
+      },
+    })
+  end,
 }
