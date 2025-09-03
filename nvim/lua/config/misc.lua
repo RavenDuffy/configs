a4fec7021881fad200c52d66b518b82f99634f2d
Raven <raven2013@gmail.com>
2025-09-03 13:52:39 +0100

    feat: added wsl copy support

---
+++ nvim/lua/config/misc.lua
@@ -0,0 +1,20 @@
+if vim.fn.has("wsl") == 1 then
+  if vim.fn.executable("wl-copy") == 1 then
+    vim.g.clipboard = {
+      name = "wl-clipboard (wsl)",
+      copy = {
+        ["+"] = "wl-copy --foreground --type text/plain",
+        ["*"] = "wl-copy --foreground --type --primary --type text/plain",
+      },
+      paste = {
+        ["+"] = (function()
+          return vim.fn.systemlist('wsl-paste --no-newline|sed -e "s/\r$//"', { '' }, 1)
+        end),
+        ["*"] = (function()
+          return vim.fn.systemlist('wsl-paste --primary --no-newline|sed -e "s/\r$//"', { '' }, 1)
+        end)
+      },
+      cache_enabled = true
+    }
+  end
+end
