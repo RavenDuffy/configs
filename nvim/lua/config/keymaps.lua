81ee64ab46c3eabeb4797e6077795784609ea636
Raven <ravend2013@gmail.com>
2025-12-02 17:53:56 +0000

    feat: no longer respecting gitignore

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,4 +1,3 @@
-
 local tele = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", tele.find_files, { desc = "Fuzz find files" })
 vim.keymap.set("n", "<leader>fg", tele.live_grep, { desc = "Grep files" })
