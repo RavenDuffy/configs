f7c80c20f6f16b049b0449562e1812e803f74396
Raven <ravend2013@gmail.com>
2026-02-10 00:50:48 +0000

    feat: updated telescope to had hidden/non-hidden searches

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,26 @@
 local tele = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", tele.find_files, { desc = "Fuzz find files" })
+
+vim.keymap.set("n", "<leader>ff", tele.find_files, { desc = "Fuzzy find files" })
+vim.keymap.set(
+  "n",
+  "<leader>xf",
+  function ()
+    tele.find_files({
+      find_command = { "rg", "--files", "--follow", "-uu", "--glob=!{**/node_modules/*,**/git/*,**/.next/*}" }
+    })
+  end,
+  { desc = "Fuzzy find files (all)" }
+)
+
 vim.keymap.set("n", "<leader>fg", tele.live_grep, { desc = "Grep files" })
+vim.keymap.set(
+  "n",
+  "<leader>xg",
+  function ()
+    tele.live_grep({
+        -- `-uu` is equivalent to `--hidden` and `--no-ignore`
+        additional_args = { "-uu", "--glob=!{**/node_modules/*,**/git/*,**/.next/*}" }
+    })
+  end,
+  { desc = "Grep files (all)" }
+)
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,31 +1,4 @@
 return {
   'nvim-telescope/telescope.nvim', tag = '0.1.8',
   dependencies = { 'nvim-lua/plenary.nvim' },
-  opts = {
-    defaults = {
-      vimgrep_arguments = {
-        "rg",
-        "--follow",
-        "--hidden",
-        "--no-heading",
-        "--with-filename",
-        "--line-number",
-        "--column",
-        "--smart-case"
-      }
-    },
-    pickers = {
-      find_files = {
-        hidden = true,
-        no_ignore = true,
-        find_command = {
-          "rg",
-          "--files",
-          "--hidden",
-          "--glob=!**/.git/*",
-          "--glob=!**/node_modules/*",
-        }
-      }
-    }
-  }
 }
